8ae23696d639ad0318b5941a059544d8e4b2e6ba
Jesus Villamarin <jesus.villamarin@getpulpo.com>
2023-08-06 08:31:24 -0500

    feat: 🎸 Create landing - cover page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 
-import Cover from '@/components/cover'
+import Cover from '@/components/main-page/cover'
+import GridSection from '@/components/main-page/grid-list-sections'
 
 
 export default function HomePage() {
   return (
     <>
       <Cover />
+      <GridSection />
 
 
     </>
